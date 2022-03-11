d29c399de9b6e5a057e838e8e03e3742d60ed6d0
Momo Jo <momo@momos-mbp.lan>
2022-03-10 23:33:13 -0800

    initial commit

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/student")
+public class StudentController {
+
+  @GetMapping
+  public String helloWord() {
+    return "Hello World";
+  }
+
+}
---
+++ src/main/java/com/example/demo/dao/StudentDao.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Student;
+import java.util.List;
+import java.util.UUID;
+
+public interface StudentDao {
+  List<Student> selectAllStudents();
+  int insertStudent(UUID id, Student student);
+
+}
